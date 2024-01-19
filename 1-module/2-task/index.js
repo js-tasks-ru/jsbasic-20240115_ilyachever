e342060c1a438d6d77df38c89a890fd08f15cb85
artur.ibragimov <artur.ibragimov@travelline.ru>
2024-01-19 21:19:47 +0400

    Fixed 2-task's soluton

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,11 +10,9 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (!name || name.length < 4) return false;
+  if (!name || name.length < 4 || name.includes(' ')) return false;
 
-  const nameWithoutSpaces = name.replaceAll(' ', '');
-
-  return nameWithoutSpaces === name;
+  return true;
 }
 
 function sayHello() {
