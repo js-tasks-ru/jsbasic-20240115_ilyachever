24dd79c14c74313997c539d340d50d77f11a52c5
artur.ibragimov <artur.ibragimov@travelline.ru>
2024-02-10 13:32:09 +0400

    Solved 5-module's tasks

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,19 @@
 function toggleText() {
-  // ваш код...
+  const trigger = document.querySelector('.toggle-text-button');
+
+  if (!trigger) return;
+
+  trigger.addEventListener('click', toggleElementVisibility);
+}
+
+function toggleElementVisibility() {
+  const textElement = document.querySelector('#text');
+
+  if (!textElement) return;
+
+  if (textElement.hasAttribute('hidden')) {
+    textElement.removeAttribute('hidden');
+  } else {
+    textElement.setAttribute('hidden', true);
+  }
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,82 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+
+  if (!carousel) return;
+
+  initialize(carousel);
+}
+
+function initialize(carousel) {
+  const carouselFeed = carousel.querySelector('.carousel__inner');
+  const slides = carouselFeed.querySelectorAll('.carousel__slide');
+  let position = 1;
+  let currentOffset = 0;
+
+  const getSlidesQuantity = () => {
+    return slides.length;
+  };
+
+  const updateControls = () => {
+    const positionVariants = {
+      min: 1,
+      max: getSlidesQuantity()
+    };
+    const prevControl = carousel.querySelector('.carousel__arrow_left');
+    const nextControl = carousel.querySelector('.carousel__arrow_right');
+
+    const hideElement = (elem) => elem.style.display = 'none';
+    const showElement = (elem) => elem.style.display = '';
+
+    switch (position) {
+    case positionVariants.min:
+      hideElement(prevControl);
+      break;
+    case positionVariants.max:
+      hideElement(nextControl);
+      break;
+    default:
+      showElement(prevControl);
+      showElement(nextControl);
+    }
+  };
+
+  const getSlideOffset = (direction, slideWidth) => {
+    switch (direction) {
+    case 'forward':
+      currentOffset -= slideWidth;
+      return currentOffset;
+    case 'backward':
+      currentOffset += slideWidth;
+      return currentOffset;
+    }
+  };
+
+  const getCurrentSlideWidth = () => {
+    return slides[position - 1].offsetWidth;
+  };
+
+  const toggleSlider = (direction) => {
+    const currentSlideWidth = getCurrentSlideWidth(carouselFeed);
+    const currentSlideOffset = getSlideOffset(direction, currentSlideWidth);
+
+    carouselFeed.style.transform = `translateX(${currentSlideOffset}px)`;
+  };
+
+  updateControls();
+
+  carousel.addEventListener('click', (evt) => {
+    const {target} = evt;
+
+    if (target.classList.contains('carousel__arrow_left')) {
+      toggleSlider('backward');
+      position--;
+    }
+
+    if (target.classList.contains('carousel__arrow_right')) {
+      toggleSlider('forward');
+      position++;
+    }
+
+    updateControls();
+  });
 }
